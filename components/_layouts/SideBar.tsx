de306434af298629e3fa04ed5d1fb01eb828cc70
Shishir Neupane <shishirs520@gmail.com>
2021-12-01 13:47:28 +0545

    sidebar added

--- components/_layouts/SideBar.tsx
+++ components/_layouts/SideBar.tsx
@@ -1,56 +1,32 @@
-import { FC, useContext, useState } from 'react';
-import { IUser } from 'schemas/user.schema';
-import { notificationContext } from '@/contextProviders/notifications/notification.context';
+import { FC } from 'react';
+import Image from 'next/image';
 
-type IProps = {
-  user: IUser;
-};
-export const SideBar: FC<IProps> = ({ user }: IProps) => {
-  const { notifications } = useContext(notificationContext);
-
+export const SideBar: FC = () => {
   return (
-    <aside className="px-0 bg-indigo-500 w-1/8">
-      <UserAvatar user={user} />
-      <UserInfo user={user} />
+    <aside className="flex flex-col px-0 bg-indigo-500 w-1/6 text-center">
+      <div className="mt-2">
+        <Image src="/icon-192x192.png" alt="user-avatar" width={80} height={80} className="rounded-full" />
+        <h3 className="mt-1 text-white">John Doe</h3>
+      </div>
 
-      <table>
-        <tbody>
-          {notifications.map((n) => {
-            return (
-              <tr key={n}>
-                <td>{n}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <nav className="mt-1 flex flex-col items-center">
+        <a href="#" className="mt-1 py-1 w-1/2 text-white border-2 rounded-xl">
+          Home
+        </a>
+        <a href="#" className="mt-1 py-1 w-1/2 text-white border-2 rounded-xl">
+          About
+        </a>
+        <a href="#" className="mt-1 py-1 w-1/2 text-white border-2 rounded-xl">
+          Features
+        </a>
+        <a href="#" className="mt-1 py-1 w-1/2 text-white border-2 rounded-xl">
+          Pricing
+        </a>
+      </nav>
+
+      <a href="#" className="border-t-2 pt-2 mt-2 text-white text-xl">
+        Logout
+      </a>
     </aside>
-  );
-};
-
-const UserAvatar = ({ user }: IProps) => {
-  return <span className="text-white border-2 border-red-50">{user.email}</span>;
-};
-
-const UserInfo = ({ user }: IProps) => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>Name:</td>
-          <td>{user.name}</td>
-        </tr>
-
-        <tr>
-          <td>Email:</td>
-          <td>{user.email}</td>
-        </tr>
-
-        <tr>
-          <td>Role:</td>
-          <td>{user.role}</td>
-        </tr>
-      </tbody>
-    </table>
   );
 };
