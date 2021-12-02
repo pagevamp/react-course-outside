ba4162fc1164638309a39727931952e21708dbca
Swejal-web <swejalshrestha08@gmail.com>
2021-12-02 11:00:20 +0545

    toast notification added with context api

--- contextProviders/notifications/notification.context.ts
+++ contextProviders/notifications/notification.context.ts
@@ -1,16 +1,25 @@
 import { createContext } from 'react';
+import { boolean, string } from 'yup/lib/locale';
 
 export type INotificationContext = {
-  showError: (msg: string) => void;
-  showSuccess: (msg: string) => void;
+  showError: (msg: string) => unknown;
+  showSuccess: (msg: string) => unknown;
   notifications: string[];
   setNotifications: (msg: string[]) => unknown;
+  notify: boolean;
+  setNotify: (msg: boolean) => unknown;
+  color: string;
+  setColor: (msg: string) => unknown;
 };
 
 export const notificationContext = createContext({
   showError: (_msg: string) => {},
   showSuccess: (_msg: string) => {},
   setNotifications: (_msg: string[]) => {},
+  notify: boolean,
+  setNotify: (_msg: boolean) => {},
+
+  setColor: (_msg: string) => {},
 } as INotificationContext);
 
 notificationContext.displayName = 'Notification';
